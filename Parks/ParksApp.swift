5ee09c9735e6d1ecb30f55fdcdcd93c069c6c901
Abhishek Jariwala <119805313+ajariwala1@users.noreply.github.com>
2023-03-08 16:32:13 -0600

    Initial Commit

---
+++ Parks/ParksApp.swift
@@ -0,0 +1,17 @@
+//
+// ParksApp.swift : Parks
+//
+// Copyright © 2023 Auburn University.
+// All Rights Reserved.
+
+
+import SwiftUI
+
+@main
+struct ParksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
